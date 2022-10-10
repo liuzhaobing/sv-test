86593871fbc72c395df3790ef3a0eca5029009f3
sevel <sevel.liu@cloudminds.com>
2022-10-10 18:21:39 +0800

    pgsql utils add

---
+++ backend/task-python/utils/utils_postgre.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+import psycopg2
+
+
+class DataBasePostGre:
+    def __init__(self, db_info):
+        self.conn = psycopg2.connect(host=db_info['host'], port=db_info['port'], user=db_info['user'],
+                                     password=db_info['password'], database=db_info['dbname'])
+        self.cursor = self.conn.cursor()
+
+    def query(self, query_string):
+        self.cursor.execute(query_string)
+        self.conn.commit()
+        return self.cursor.fetchall()
+
+    def __del__(self):
+        self.cursor.close()
+        self.conn.close()
