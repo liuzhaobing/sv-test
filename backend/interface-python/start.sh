5ba54fba023cce138501ff31c015eb3d9ed5bd1c
sevel.liu@cloudminds.com <sevel.liu@cloudminds.com>
2023-02-16 20:41:06 +0800

    1.init start

--- backend/interface-python/start.sh
+++ backend/interface-python/start.sh
@@ -5,4 +5,4 @@ docker stop interface_python
 docker rm interface_python
 docker rmi interface_python:1.0
 docker build -t interface_python:1.0 .
-docker run -d -p 27991:8090 -v /etc/localtime:/etc/localtime --name=interface_python --restart=always interface_python:1.0
+docker run -d -p 27998:8090 -v /etc/localtime:/etc/localtime --name=interface_python --restart=always interface_python:1.0
