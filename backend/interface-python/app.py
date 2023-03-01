3bcdc0dcd32e362a83fb01aec5632b3a98f36c49
sevel <sevel.liu@cloudminds.com>
2023-03-01 17:33:47 +0800

    1.add(新增数据库写入执行历史)

--- backend/interface-python/app.py
+++ backend/interface-python/app.py
@@ -2,11 +2,13 @@
 import json
 import logging
 import os
+from datetime import datetime
 
 from flask import Flask
 from flask import make_response
 from flask import request
 from google.protobuf import json_format
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 logger = logging.getLogger('gunicorn.error')
@@ -15,60 +17,151 @@ UPLOAD_FOLDER = ''
 ALLOWED_EXTENSIONS = {'proto'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root@172.16.23.33:3306/nlpautotest"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+db = SQLAlchemy(app)
+
+
+class ProtoManagement(db.Model):
+    __tablename__ = "proto_management"
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    proto_name_zh = db.Column(db.String(255))
+    proto_name_en = db.Column(db.String(255))
+    proto_content = db.Column(db.Text)
+    last_access_time = db.Column(db.DATETIME, default=datetime.now())
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "proto_name_zh": self.proto_name_zh,
+            "proto_name_en": self.proto_name_en,
+            "proto_content": self.proto_content,
+            "last_access_time": self.last_access_time.strftime("%Y-%m-%d %H:%M:%S")
+        }
+
+
+class ProtoManagementLog(db.Model):
+    __tablename__ = "proto_management_log"
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    proto_name_en = db.Column(db.String(255))
+    last_access_address = db.Column(db.String(255))
+    last_access_function = db.Column(db.String(255))
+    last_access_request = db.Column(db.Text)
+    last_access_response = db.Column(db.Text)
+    last_access_time = db.Column(db.DATETIME, default=datetime.now())
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "proto_name_en": self.proto_name_en,
+            "last_access_address": self.last_access_address,
+            "last_access_function": self.last_access_function,
+            "last_access_request": self.last_access_request,
+            "last_access_response": self.last_access_response,
+            "last_access_time": self.last_access_time.strftime("%Y-%m-%d %H:%M:%S")
+        }
+
+
+@app.route('/proto', methods=['GET', 'POST'])
+def proto_management():
+    if request.method == "GET":
+        result = ProtoManagement.query.order_by(ProtoManagement.last_access_time.desc())
+        return make_response({"status": "success", "data": [item.to_dict() for item in result]}, 200)
+    if request.method == "POST":
+        data = request.get_data()
+        json_re = json.loads(data)
+        check_proto_use_history = ProtoManagement.query.filter_by(proto_name_en=json_re["proto_name_en"]).count()
+        write_local_file(json_re["proto_name_en"], json_re["proto_content"])
+        gen_res, cmd = generate_pb(json_re["proto_name_en"])
+        if gen_res != 0:
+            # 判断pb文件生成是否成功
+            return make_response({"status": "failure", "error": f'execute command error: {cmd}', "code": gen_res}, 200)
+
+        if not check_proto_use_history:
+            # 判断之前是否有存储过此proto文件
+            new_proto = ProtoManagement(proto_name_zh=json_re["proto_name_zh"],
+                                        proto_name_en=json_re["proto_name_en"],
+                                        proto_content=json_re["proto_content"])
+            result = db.session.add(new_proto)
+            return make_response({"status": "success", "data": result}, 200)
+
+        result = ProtoManagement.query.filter_by(proto_name_en=json_re["proto_name_en"]).update(json_re)
+        return make_response({"status": "success", "data": result}, 200)
+
+
+@app.route('/proto/<int:nid>', methods=['GET', 'PUT', 'DELETE'])
+def proto_management_crud(nid):
+    if request.method == "GET":
+        result = ProtoManagement.query.filter_by(id=nid).first()
+        return make_response({"status": "success", "data": result.to_dict()}, 200)
+
+    if request.method == "DELETE":
+        result = ProtoManagement.query.filter_by(id=nid).delete()
+        return make_response({"status": "success", "data": result}, 200)
+
+    if request.method == "PUT":
+        data = request.get_data()
+        json_re = json.loads(data)
+        write_local_file(json_re["proto_name_en"], json_re["proto_content"])
+        gen_res, cmd = generate_pb(json_re["proto_name_en"])
+        if gen_res != 0:
+            # 判断pb文件生成是否成功
+            return make_response({"status": "failure", "error": f'execute command error: {cmd}', "code": gen_res}, 200)
+        result = ProtoManagement.query.filter_by(id=nid).update(json_re)
+        return make_response({"status": "success", "data": result}, 200)
+
+
+@app.route('/proto/logs', methods=['GET', 'POST'])
+def proto_management_log():
+    if request.method == "GET":
+        result = ProtoManagementLog.query.order_by(ProtoManagementLog.last_access_time.desc())
+        return make_response({"status": "success", "data": [item.to_dict() for item in result]}, 200)
+    if request.method == "POST":
+        data = request.get_data()
+        json_re = json.loads(data)
+        new_proto = ProtoManagementLog(proto_name_en=json_re["proto_name_en"],
+                                       last_access_address=json_re["last_access_address"],
+                                       last_access_function=json_re["last_access_function"],
+                                       last_access_request=json_re["last_access_request"],
+                                       last_access_response=json_re["last_access_response"])
+        result = db.session.add(new_proto)
+        return make_response({"status": "success", "data": result}, 200)
+
+
+@app.route('/proto/logs/<proto_name_en>', methods=['GET', 'PUT', 'DELETE'])
+def proto_management_log_crud(proto_name_en):
+    if request.method == "GET":
+        result = ProtoManagementLog.query.filter_by(proto_name_en=proto_name_en).first()
+        return make_response({"status": "success", "data": result.to_dict()}, 200)
+
+    if request.method == "DELETE":
+        result = ProtoManagementLog.query.filter_by(proto_name_en=proto_name_en).delete()
+        return make_response({"status": "success", "data": result}, 200)
+
+    if request.method == "PUT":
+        data = request.get_data()
+        json_re = json.loads(data)
+        result = ProtoManagementLog.query.filter_by(proto_name_en=proto_name_en).update(json_re)
+        return make_response({"status": "success", "data": result}, 200)
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-@app.route('/files', methods=['POST', ])
-def upload():
-    """{"file": file.proto}"""
-    if request.method == 'POST':
-        # 检查文件是否已经上传
-        if 'file' not in request.files:
-            return make_response({"status": "failure", "error": "upload file failed!"}, 500)
-
-        file = request.files['file']
-        # 如果用户没有选择文件，则提交一个空表单
-        if file.filename == '':
-            return make_response({"status": "failure", "error": "not find file!"}, 500)
-
-        # 如果文件是允许的类型，则保存文件
-        if file and allowed_file(file.filename):
-            file.save(file.filename)
-            return make_response({"status": "success", "error": ""}, 200)
-        return make_response({"status": "failure", "error": "not support file type!"}, 500)
+def write_local_file(filename: str, file_content: str):
+    with open(filename, "w") as f:
+        f.write(file_content)
 
 
-@app.route('/files', methods=['GET', ])
-def list_proto_files():
-    files = os.listdir()
-    return make_response({"status": "success", "data": [i for i in files if allowed_file(i)]}, 200)
+def check_file_exists(filename: str) -> bool:
+    return os.path.exists(filename)
 
 
 def generate_pb(proto_file):
     cmd = f'python -m grpc_tools.protoc -I. -I./src --python_out=. --grpc_python_out=. {proto_file}'
     return os.system(cmd), cmd
-
-
-@app.route('/generate', methods=['POST', ])
-def generate_pb_interface():
-    data = request.get_data()
-    json_re = json.loads(data)
-    if not allowed_file(json_re["filename"]):
-        return make_response({"status": "failure", "error": f'type error! only support "*.proto"'}, 500)
-
-    if not json_re.__contains__("filename"):
-        return make_response({"status": "failure", "error": f'key error! only support key "filename"'}, 500)
-
-    if not os.path.exists(json_re["filename"]):
-        return make_response({"status": "failure", "error": f'not find file: {json_re["filename"]}'}, 500)
-
-    result, cmd = generate_pb(json_re["filename"])
-    if result != 0:
-        return make_response({"status": "failure", "error": f'execute command error: {cmd}', "code": result}, 500)
-    return make_response({"status": "success", "error": ""}, 200)
 
 
 @app.route('/interface', methods=['POST', ])
@@ -118,15 +211,17 @@ result = ins.call(message=pb2.{req_func}(), func=ins.stub.{call_func}, payload={
 exec_result["result"] = result
 exec_result["cost"] = int((time.time() - start_time) * 1000)
         """
+
     if not request.content_type.startswith("application/json"):
-        return make_response(json.dumps({"status": "failure", "error": "not allowed content type!"}, ensure_ascii=False), 500)
+        return make_response(
+            json.dumps({"status": "failure", "error": "not allowed content type!"}, ensure_ascii=False), 500)
 
     data = request.get_data()
     json_re = json.loads(data)
 
     def pb_to_json(pb):
         return json_format.MessageToJson(pb)
-    import os
+
     proto_file = json_re["proto"] if json_re.__contains__("proto") else "abc.proto"
     proto_file_pb2 = proto_file.split(".")[0] + "_pb2.py"
     proto_file_pb2_grpc = proto_file.split(".")[0] + "_pb2_grpc.py"
@@ -144,18 +239,27 @@ exec_result["cost"] = int((time.time() - start_time) * 1000)
     sc = template(proto=proto_file.split(".")[0],
                   url=f"'{url}'",
                   payload=json_re["payload"],
-                  stub=func.split(".")[0],
+                  stub=func.split(".")[0] + "Stub",
                   req_func=func.split(".")[1].split("(")[1].split(")")[0],
                   call_func=func.split(".")[1].split("(")[0],
                   request_stream=json_re["stream_in"],
                   response_stream=json_re["stream_out"])
     try:
         exec(sc, {"exec_result": res})
+        response_body = json.loads(pb_to_json(res["result"])) if json_re["stream_out"] == "False" \
+            else [json.loads(pb_to_json(r)) for r in res["result"]]
+        response_json = {"cost": res["cost"], "data": response_body}
+        database_log = ProtoManagementLog()
+        database_log.proto_name_en = json_re["proto"]
+        database_log.last_access_address = json_re["url"]
+        database_log.last_access_function = func
+        database_log.last_access_request = json.dumps(json_re["payload"], ensure_ascii=False)
+        database_log.last_access_response = json.dumps(response_json, ensure_ascii=False)
+        db.session.add(database_log)
+        return make_response(response_json, 200)
     except:
-        return make_response({"status": "failure", "error": traceback.format_exc()}, 200)
-    if json_re["stream_out"] == "False":
-        return make_response({"cost": res["cost"], "data": json.loads(pb_to_json(res["result"]))}, 200)
-    return make_response({"cost": res["cost"], "data": [json.loads(pb_to_json(r)) for r in res["result"]]}, 200)
+        response_json = {"status": "failure", "error": traceback.format_exc()}
+        return make_response(response_json, 200)
 
 
 if __name__ != '__main__':
